cb28e0e9e10513701b908da4b8cd23581fd00d13
DevinForr <devinrforrest@gmail.com>
2025-04-01 14:19:37 -0400

    small edit

--- MiniProject/MiniProject/views.py
+++ MiniProject/MiniProject/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django import forms
+from django.http import JsonResponse
+
 
 def login(request):
     print("login\n")
@@ -18,3 +20,22 @@ def instructor_home(request):
 def instructor_block(request):
     print("instructor_block\n")
     return render(request, 'instructor_block.html')
+
+def get_instructor_info(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        department = request.POST['department']
+        email = request.POST['email']
+
+        # Redirect to welcome page with user's data passed in URL or session
+        request.session['first_name'] = first_name
+        request.session['last_name'] = last_name
+        return redirect('welcome')
+    return render(request, 'user_form.html')
+
+def welcome_view(request):
+    # Retrieve user data from session
+    first_name = request.session.get('first_name', 'Guest')
+    
+    return render(request, 'instructor_block.html', {'first_name': first_name})
